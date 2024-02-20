9382d2c4836f9d1eebf753ac61a4c11c28a82ee3
Mustapha <mustapha.yinusa@yahoo.com>
2024-02-21 00:28:19 +0100

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,27 +11,27 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int tmp;
 	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
-
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
 				swapped = 1;
-
 				print_array(array, size);
 			}
 		}
-
-		if (!swapped)
+		if (swapped == 0)
 			break;
 	}
 }
